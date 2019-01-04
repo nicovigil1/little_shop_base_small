ee020af08c82144ed33b9efaf651785be51f8d80
Nico <nicovigil1@gmail.com>
2019-01-04 11:52:16 -0700

    adds migration for discounts table

---
+++ db/migrate/20190104184550_create_discounts.rb
@@ -0,0 +1,12 @@
+class CreateDiscounts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :discounts do |t|
+      t.integer :amount_off
+      t.int4range :quantity
+      t.integer :item_total
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
