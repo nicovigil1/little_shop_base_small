439103bbe77f52c6d856b693a189ab4a0d1d1a25
Nico <nicovigil1@gmail.com>
2019-01-05 23:22:05 -0700

    adds passing validation test for user relation to discount

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe User, type: :model do
     it { should have_many :items }
     it { should have_many :orders }
     it { should have_many(:order_items).through(:orders) }
+    it { should have_many :discounts }
   end
 
   describe 'class methods' do
