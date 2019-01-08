0435bf1d9cad1cdaab811706c542060656d117f3
Nico <nicovigil1@gmail.com>
2019-01-08 14:17:12 -0700

    adds passing tests for discount creation and display by users preferences of discount

--- app/controllers/dashboard/discounts_controller.rb
+++ app/controllers/dashboard/discounts_controller.rb
@@ -1,7 +1,74 @@
 class Dashboard::DiscountsController < Dashboard::BaseController
     
     def index
-        @discounts = current_user.discounts
+        @discounts = current_user.discounts_by_type(current_user.discount_type)
+    end
+
+    def new
+        @discount = Discount.new
+        params[:discount] ? set_discount_type_session(params[:discount]) : nil
+        if current_user.discount_type == 2 && session[:discount_type] == nil
+            redirect_to dashboard_set_type_path
+        elsif discount_type(0)
+            current_user.update(discount_type: 0)
+            render partial: 'dashboard/discounts/percent_discount_form', layout: "application"
+        elsif discount_type(1)
+            current_user.update(discount_type: 1)
+            render partial: 'dashboard/discounts/dollar_discount_form', layout: "application"
+        end 
     end 
 
+    def create
+        if quantity_discount
+            new_discount = Discount.create(discount_params)
+            new_discount.update(kind: 0)
+            redirect_to dashboard_discounts_path
+        elsif dollar_discount
+            new_discount = Discount.create(discount_params)
+            new_discount.update(kind: 1)
+            redirect_to dashboard_discounts_path
+        end 
+    end
+
+    def set_type
+    end 
+
+    def toggle_discount
+        chosen_discount = current_user.discount_type 
+        chosen_discount == 0 ? chosen_discount = 1 : chosen_discount = 0 
+        current_user.update(discount_type: chosen_discount)
+        session[:discount_type] = chosen_discount
+        redirect_to dashboard_discounts_path
+    end
+
+    private 
+
+    def discount_params
+        if quantity_discount
+            discount = params.require(:discount).permit(:amount_off, :max_quantity, :min_quantity)
+            discount[:quantity] = discount[:min_quantity]...discount[:max_quantity]
+            discount[:user] = current_user 
+            discount.except(:min_quantity, :max_quantity)
+        elsif dollar_discount
+            discount = params.require(:discount).permit(:amount_off, :item_total)
+            discount[:user] = current_user
+            discount
+        end
+    end 
+
+    def set_discount_type_session(numba)
+        session[:discount_type] = numba.to_i 
+    end 
+
+    def quantity_discount
+        params[:discount][:min_quantity]
+    end
+    
+    def dollar_discount
+        params[:discount][:item_total]
+    end 
+
+    def discount_type(type)
+        session[:discount_type] == type || current_user.discount_type == type
+    end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,9 @@ Rails.application.routes.draw do
       post '/items/:id/fulfill', to: 'orders#fulfill_item', as: 'item_fulfill'
     end
 		resources :items, except: [:show]
-		resources :discounts, only: [:index]
+    resources :discounts, only: [:index, :new, :create]
+    put '/discounts/toggle_discount', to: "discounts#toggle_discount", as: 'toggle_discount'
+    get "/discounts/set-type", to: "discounts#set_type", as: 'set_type'
     patch '/items/:id/enable', to: 'items#enable', as: 'enable_item'
     patch '/items/:id/disable', to: 'items#disable', as: 'disable_item'
   end
