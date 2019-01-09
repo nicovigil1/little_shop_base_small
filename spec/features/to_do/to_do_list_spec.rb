8217f207475c1b27708ac44120e4679e3ba3692c
Nico <nicovigil1@gmail.com>
2019-01-08 20:30:30 -0700

    adds methods for finding and displaying a count of a merchants unsold orders, as well as how much revenue they stand to gain

--- spec/features/to_do/to_do_list_spec.rb
+++ spec/features/to_do/to_do_list_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 describe "on the dashboard" do
     it 'tells you which images you need to add a thumbnail for' do 
         merchant = create(:merchant)
-        item1= create(:item, image: nil)
-        item2 = create(:item, image: nil)
-        item3 = create(:item, image: nil)
+        item1= create(:item, image: 'https://picsum.photos/200/300/?image=524', user: merchant)
+        item2 = create(:item, image: 'https://picsum.photos/200/300/?image=524', user: merchant)
+        item3 = create(:item, image: 'https://picsum.photos/200/300/?image=524', user: merchant)
         item4 = create(:item)
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
@@ -12,32 +12,32 @@ describe "on the dashboard" do
         visit dashboard_path
 
         within "#to-do" do 
-            expect(page).to have_content(item1.name) 
-            expect(page).to have_content(item2.name) 
-            expect(page).to_not have_content(item4.name)
-            click_on item_1.name
+            expect(page).to have_link(item1.name) 
+            expect(page).to have_link(item2.name) 
+            expect(page).to_not have_link(item4.name)
+            click_on item1.name
         end 
 
         expect(current_path).to eq(edit_dashboard_item_path(item1))
     end 
 
-    xit "shows you unfufilled items & how much money you're missing out on" do
+    it "shows you unfufilled items & how much money you're missing out on" do
         merchant = create(:merchant)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
         order = create(:order)
-        item1 = create(:item)
-        item2 = create(:item)
-        item3 = create(:item)
+        item1 = create(:item, user: merchant)
+        item2 = create(:item, user: merchant)
+        item3 = create(:item, user: merchant)
         oi_1 = create(:order_item, item: item1, order: order)
         oi_2 = create(:order_item, item: item2, order: order)
-        oi_3 = create(:fufilled_order_item, item: item3, order: order)
+        oi_3 = create(:fulfilled_order_item, item: item3, order: order)
 
         visit dashboard_path
 
-        within "#to-do-fufilled" do 
-            expect(page).to have_content(oi_1.name) 
-            expect(page).to have_content(oi_2.name) 
-            expect(page).to_not have_content(oi_3.name)
+        within "#to-do-fulfilled" do 
+            expect(page).to have_content(merchant.unfulfilled_orders_count) 
+            expect(merchant.unfulfilled_orders_count).to eq(2)
+            expect(page).to have_content(merchant.unfulfilled_orders_revenue) 
         end 
     end 
 end
